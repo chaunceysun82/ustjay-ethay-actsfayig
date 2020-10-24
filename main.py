aae49bab492571c2cfc4f44185db62ad3e5215f9
chaunceysun82 <chauncey.sun1982@gmail.com>
2020-10-24 16:49:04 -0700

    initial commit. 4:48pm, 10/24/2020

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, redirect
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -19,8 +19,19 @@ def get_fact():
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    host = 'https://hidden-journey-62459.herokuapp.com'
+    path = '/piglatinize/'
+    request_url = host + path
 
+    text = get_fact()
+    data = {"input_text": text}
+
+    response = requests.post(request_url, data=data, allow_redirects=False)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    new_path = soup.find('a').text
+    response_url = host + new_path
+
+    return response_url
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
